5232c8d0113ddadedee4d22b4cc62c829c9d0d5b
gatilhoroxo <gatilhoroxo@gmail.com>
2025-11-11 15:32:56 -0800

    chore(setup.bash) e feat(.gitmodules e contiki)

--- setup/setup.bash
+++ setup/setup.bash
@@ -1,3 +1,22 @@
+sudo apt update
+sudo apt upgrade
+sudo apt-get install git build-essential doxygen curl wireshark  binutils-msp430 gcc-msp430 msp430-libc msp430mcu mspdebug gcc-arm-none-eabi openjdk-8-jdk openjdk-8-jre ant libncurses5-dev avr-libc gcc-avr
+
+sudo update-alternatives --config java
+# escolher a versao 8
+
+sudo snap install code --classic
+
+sudo snap install gh
+
+git clone https://github.com/gatilhoroxo/contiki.git
+git clone https://github.com/gatilhoroxo/tarc_attempt.git
+
+cd contiki/tools/cooja
+sudo ant run
+
+
+
 #APENAS PARA VERSAO 20.04 DO UBUNTU
 # essa parte eh soh se for fazer o install do zero com o .sh
 #chmod +x install.sh
@@ -13,7 +32,3 @@
 #rm -rf .git/modules/platform/stm32nucleo-spirit1
 #git submodule sync
 #git submodule update --init --recursive
-
-unzip contiki.zip
-cd contiki/tools/cooja
-sudo ant run
