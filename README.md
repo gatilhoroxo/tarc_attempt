f8fd51fef8664674d3003cf1c10d0f0d69397268
gatilhoroxo <gatilhoroxo@gmail.com>
2025-11-20 19:16:28 -0300

    chore: ajusting files vm_problems.sh, .gitignore and README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tarc_attempt
 Cadeira de Topicos Avancados em Redes de Computadores com os professores Renato Mariz e Paulo Filipe
+
+
+## 📝 Anotações
+
+Use este espaço para suas anotações durante o desenvolvimento...
+
+**2025-11-19:** 
+Encontrei essa playlist para colocar o plugin de mobilidade no cooja <a href="https://www.youtube.com/playlist?list=PLjUt6bve4O-flgRCMG54sLkXyABqBTsh-">mobility</a>
+
+---
--- setup/vm_problems.sh
+++ setup/vm_problems.sh
@@ -1,5 +1,13 @@
-# se nao quiser quebrar a cabeça com isso
-#   apenas desative o secure boot temporariamente
+# =======================================================
+# estes comandos é apenas se estiver usando o vmware no ubuntu 24 
+# se estiver na sua própria máquina e com a versão do ubuntu 20, de boa! pode ignorar esse arquivo
+# =======================================================
+
+# -------------------------------------------------------
+# caso estiver usando o vmware e ele estiver rodando a maquina virtual, mas a internet nao está conectando e
+# voce nao quer quebrar a cabeça com isso,
+#   apenas desative o secure boot temporariamente para usar a maquina virtual quando precisar usar internet dentro dela
+# -------------------------------------------------------
 
 # gerando e importando uma chave
 openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=VMware/"
